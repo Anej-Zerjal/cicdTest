465fc1f51ff3dea14abdc6c3aebc75d71895d028
F3D-up <80313567+F3D-up@users.noreply.github.com>
2025-05-25 19:03:57 +0200

    fix 7

--- test/test_test.py
+++ test/test_test.py
@@ -1,10 +1,12 @@
 # Your test file (e.g., test_my_integration.py)
+from kronoterm_voice_actions.wyoming import matcher
 
 import pytest
 
 
 # This test will NOT be marked
 def test_basic_functionality():
+    parsed_number = matcher.parse_slovene_number("dvajset in tri")
     assert 1 + 1 == 2
     print("Basic functionality test completed successfully")
 
