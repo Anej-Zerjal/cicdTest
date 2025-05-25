99318197f8448923a7d7cfe5bdc17366f68d2530
F3D-up <80313567+F3D-up@users.noreply.github.com>
2025-05-25 16:28:30 +0200

    custom agent automatic config

--- src/kronoterm_voice_actions/wyoming/config_flow.py
+++ src/kronoterm_voice_actions/wyoming/config_flow.py
@@ -119,10 +119,12 @@ class WyomingConfigFlow(ConfigFlow, domain=DOMAIN):
             return await self.async_step_remote_service()
 
         if self._entry_type == ENTRY_TYPE_CUSTOM:
-            await self.async_set_unique_id(CUSTOM_AGENT_UNIQUE_ID)
-            self._abort_if_unique_id_configured()
-            return await self.async_step_custom_agent_auth()
-            # return self.async_abort(reason="unknown_entry_type")
+            return self.async_create_entry(
+                title="Kronoterm Conversation Agent",
+                data={
+                    CONF_TYPE: ENTRY_TYPE_CUSTOM,
+                },
+            )
 
         return self.async_abort(reason="unknown_entry_type")
 
