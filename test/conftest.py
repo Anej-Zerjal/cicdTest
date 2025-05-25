e9758e1ac695d1a767a0c77986dd8e2716aca26a
F3D-up <80313567+F3D-up@users.noreply.github.com>
2025-05-25 20:10:16 +0200

    revert

--- test/conftest.py
+++
@@ -1,8 +0,0 @@
-# test/conftest.py
-import pytest
-
-
-def pytest_configure(config):
-    config.addinivalue_line(
-        "markers", "target_machine: mark test to run on the target machine"
-    )
