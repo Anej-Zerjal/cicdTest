18b96d4d6a2f6b3ef138435ac497230b1ea0d800
F3D-up <80313567+F3D-up@users.noreply.github.com>
2025-05-25 21:30:10 +0200

    fix 29

--- src/kronoterm_voice_actions/test/test_matcher.py
+++ src/kronoterm_voice_actions/test/test_matcher.py
@@ -1,7 +1,7 @@
 # src/kronoterm_voice_actions/test/test_matcher.py
 
 # Import relative to the package structure
-from kronoterm_voice_actions.wyoming import matcher
+from wyoming import matcher
 
 import pytest
 
