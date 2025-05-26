9685b74763faa72bf3e3032382c67fa6e789849e
F3D-up <80313567+F3D-up@users.noreply.github.com>
2025-05-26 02:36:54 +0200

    my bad

--- src/kronoterm_voice_actions/test/test_matcher.py
+++ src/kronoterm_voice_actions/test/test_matcher.py
@@ -96,7 +96,7 @@ def test_parse_slovene_number_typos():
 def test_replace_numbers():
     assert matcher.replace_numbers_with_digits("nastavi temperaturo na dvajset stopinj") == "nastavi temperaturo na 20.0 stopinj"
     assert matcher.replace_numbers_with_digits("nastavi na pet in dvajset stopinj") == "nastavi na 5.0 in 20.0 stopinj"
-    assert matcher.replace_numbers_with_digits("ena dva tri") == "1 2 3"
+    assert matcher.replace_numbers_with_digits("ena dva tri") == "1.0 2.0 3.0"
     assert matcher.replace_numbers_with_digits("dve celi pet") == "2.5"
 
 # Test includes_temperature
